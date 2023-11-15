ca1d87421fad38e7e8c215fd05c8391a2376a788
naomikonlack <96690082+naomikonlack@users.noreply.github.com>
2023-11-15 22:01:14 +0000

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from tensorflow.keras.models import load_model
+from joblib import load
 
 # Load the saved components
 model = load_model('model.joblib')
